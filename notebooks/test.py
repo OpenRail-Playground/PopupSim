49aaa471d1f36d7a685018b7da4a38ccbba88295
Sven Stauden <sven.stauden@deutschebahn.com>
2024-10-01 15:30:48 +0200

    added wagons

--- notebooks/test.py
+++ notebooks/test.py
@@ -4,33 +4,33 @@ import simpy
 # %%
 
 DRIVE_TIMES = {
-    "a" : {
-        "b1": 5,
-        "b2": 5,
-        "c": 10,
+    "toBeRetrofitted" : {
+        "WorkshopGleis0": 5,
+        "WorkshopGleis1": 5,
+        "retrofitted": 10,
         "kopf": 5
     },
-    "b1" : {
-        "a": 5,
-        "c": 10,
+    "WorkshopGleis0" : {
+        "toBeRetrofitted": 5,
+        "retrofitted": 10,
         "kopf": 5
     },
-    "b2" : {
-        "a": 5,
-        "c": 10,
+    "WorkshopGleis1" : {
+        "toBeRetrofitted": 5,
+        "retrofitted": 10,
         "kopf": 5
     },
-    "c" : {
-        "a": 10,
-        "b1": 5,
-        "b2": 5,
+    "retrofitted" : {
+        "toBeRetrofitted": 10,
+        "WorkshopGleis0": 5,
+        "WorkshopGleis1": 5,
         "kopf": 5
     },
     "kopf" : {
-        "a": 5,
-        "b1": 5,
-        "b2": 5,
-        "c": 10,
+        "toBeRetrofitted": 5,
+        "WorkshopGleis0": 5,
+        "WorkshopGleis1": 5,
+        "retrofitted": 10,
         "kopf": 5
     },
 
@@ -48,8 +48,11 @@ class track(object):
 
   def log(self):
      return {
-        self.name: self.wagons
+        self.name: [wagon.log() for wagon in self.wagons],
      }
+  
+  def add_wagon(self, wagon):
+     self.wagons = [wagon] + self.wagons
   
 
 class wagon(object):
@@ -70,13 +73,13 @@ class wagon(object):
 
 
 class locomotive(object):
-    def __init__(self, env, global_setting, id=1):
+    def __init__(self, env, global_setting, start_track, id=1):
         self.env = env
         self.global_setting = global_setting
 
         self.id = id
         self.coupled_with = []
-        self.position = "kopf"
+        self.cur_track = start_track
 
         # Start the run process everytime an instance is created.
         self.action = env.process(self.run())
@@ -84,8 +87,8 @@ class locomotive(object):
     def log(self):
       return {
           "lokomotiveId": self.id,
-          "coupledWith:": self.coupled_with,
-          "position": self.position,
+          "coupledWith:": [wagon.log() for wagon in self.coupled_with],
+          "position": self.cur_track.name,
         }
 
     def run(self):
@@ -93,8 +96,9 @@ class locomotive(object):
         self.global_setting.log_global_state()
 
         # drive to b1
+        self.global_setting.tracks.workshop_tracks[0]
         
-        yield self.env.process(self.drive_to("b1"))
+        yield self.env.process(self.drive_to(self.global_setting.tracks.workshop_tracks[0]))
        
 
 
@@ -103,7 +107,7 @@ class locomotive(object):
 
         # drive to C
         
-        yield self.env.process(self.drive_to("c"))
+        yield self.env.process(self.drive_to(self.global_setting.tracks.retrofitted))
         
 
         # uncoupling
@@ -113,7 +117,7 @@ class locomotive(object):
 
         # drive to from C to a
         
-        yield self.env.process(self.drive_to("a"))
+        yield self.env.process(self.drive_to(self.global_setting.tracks.toBeRetrofitted))
         
 
         # coupling
@@ -121,7 +125,7 @@ class locomotive(object):
 
         # drive from a to b1
         
-        yield self.env.process(self.drive_to("b1"))
+        yield self.env.process(self.drive_to(self.global_setting.tracks.workshop_tracks[0]))
         
 
         # uncoupling
@@ -129,7 +133,7 @@ class locomotive(object):
 
         # drive from b1 to kopf
         
-        yield self.env.process(self.drive_to("kopf"))
+        yield self.env.process(self.drive_to(self.global_setting.tracks.head_track))
        
 
         self.global_setting.log_global_state()
@@ -138,26 +142,54 @@ class locomotive(object):
     def coupling(self, duration=5):
         print('start coupling %d' % self.env.now)
         yield self.env.timeout(duration)
+        self.coupled_with += self.cur_track.wagons[:3]
         print('finished coupling %d' % self.env.now)
+
         self.global_setting.log_global_state()
     
     def uncoupling(self, duration=5):
         print('start uncoupling %d' % self.env.now)
         yield self.env.timeout(duration)
+        self.coupled_with = []
         print('finished uncoupling %d' % self.env.now)
         self.global_setting.log_global_state()
     
-    def drive_to(self, target):
-        print(f"start driving from {self.position} to {target} - {self.env.now}")
+    def drive_to(self, target_track):
+        print(f"start driving from {self.cur_track.name} to {target_track.name} - {self.env.now}")
   
         # compute driving time
-        driving_time = DRIVE_TIMES[self.position][target]
+        driving_time = DRIVE_TIMES[self.cur_track.name][target_track.name]
         yield self.env.timeout(driving_time)
-        self.position = target
+        
+        for wagon in self.coupled_with:
+          # remove wagons from old track
+          self.cur_track.wagons.remove(wagon)
+
+        # add to new track
+        target_track.wagons = self.coupled_with + target_track.wagons
+        
+        # change track of locomotive
+        self.cur_track = target_track
   
-        print(f"arrived {target} - {self.env.now}")
+        print(f"arrived {target_track.name} - {self.env.now}")
         self.global_setting.log_global_state()
 # %%
+class TrackCollection(object):
+  def __init__(self, env) -> None:
+      self.head_track = track(env, "kopf")
+      self.retrofitted = track(env, "retrofitted")
+      self.toBeRetrofitted = track(env, "toBeRetrofitted")
+      self.workshop_tracks = [
+         track(env, f"WorkshopGleis{i}") for i in range(2)
+      ]
+
+  def log(self):
+      logs = self.retrofitted.log()
+      logs.update(self.toBeRetrofitted.log())
+      logs.update({"workshopGleise":[ workshop_track.log() for workshop_track in self.workshop_tracks]})
+      return logs
+    
+
 
 class globalSetting(object):
 
@@ -165,22 +197,23 @@ class globalSetting(object):
    
     self.env = simpy.Environment()
 
-    # create locomotive
-    self.locomotive = locomotive(self.env, self)
+    # create tracks
+    self.tracks = TrackCollection(self.env)
 
     # create wagons
     self.wagons = []
     for i in range(20):
       self.wagons.append(wagon(self.env, i))
 
-    # create tracks
-    self.retrofitted_track = track(self.env, "retrofitted")
-    self.toBeRetrofitted = track(self.env, "toBeRetrofitted")
+    # add wagons to tracks
+    self.tracks.workshop_tracks[0].wagons = self.wagons[:3]
+    self.tracks.workshop_tracks[1].wagons = self.wagons[3:6]
+    self.tracks.toBeRetrofitted.wagons = self.wagons[6:]
+
+    # create locomotive
+    self.locomotive = locomotive(self.env, self, self.tracks.head_track)
 
     # create workshop tracks
-    self.workshop_tracks = []
-    for i in range(2):
-      self.workshop_tracks.append(track(self.env, f"WorkshopGleis{i}"))
 
   
     self.global_log = []
@@ -192,10 +225,8 @@ class globalSetting(object):
      self.global_log.append(
         {
            "locomotive": self.locomotive.log(),
-           "retrofitted": self.retrofitted_track.log(),
-           "toBeRetrofitted": self.toBeRetrofitted.log(),
-           "workshopGleise":[ workshop_track.log() for workshop_track in self.workshop_tracks],
-           "timestamp": self.env.now
+           "timestamp": self.env.now,
+           "tracks": self.tracks.log()
         }
      )
     
